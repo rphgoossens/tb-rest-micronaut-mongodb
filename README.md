5976e1b7fff71db1cf5542c7a8492b151c3972f3
rphgoossens <r.p.h.goossens@gmail.com>
2020-08-15 11:28:46 +0200

    Added OpenAPI

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 - [Micronaut Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
 
+### Testing
+This project uses mongodb to store its data. You can spin it up via Docker:
+```bash
+docker run -d -p 27017-27019:27017-27019 --name mongodb mongo
+```
--- src/main/java/nl/terrax/tbrestmongodb/Application.java
+++ src/main/java/nl/terrax/tbrestmongodb/Application.java
@@ -1,7 +1,15 @@
 package nl.terrax.tbrestmongodb;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "tb-rest-micronaut-mongodb",
+                version = "1.0"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
